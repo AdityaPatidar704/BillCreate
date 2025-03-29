c4293402c580705c574c61d6b6884b2357dbb4fb
Aditya Patidar <adityapatidar7049@gmail.com>
2025-03-29 15:24:24 +0530

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Sidebar from '../container/Sidebar'
+import Sidebar from '../layouts/Sidebar'; // Make sure Sidebar is correctly imported
+const Dashboard = () => {
+  return (
+    <div>
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Dashboard
